c8ba8cde170777007bd9a9d5a1215d2e83153606
Congyuwang <congyuwang@link.cuhk.edu.cn>
2021-10-02 14:05:47 +0800

    change to hashmap for performance

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use bitcoin_explorer::api::{BitcoinDB, Address, SConnectedBlock};
+use bitcoin_explorer::{BitcoinDB, Address, SConnectedBlock};
 use chrono::{Date, NaiveDateTime, Utc};
 use indicatif;
 use indicatif::ProgressStyle;
 use log::info;
 use simple_logger::SimpleLogger;
 use siphasher::sip128::{Hasher128, SipHasher13};
-use std::collections::{BTreeMap, HashMap};
+use std::collections::HashMap;
 use std::fs;
 use std::fs::File;
 use std::hash::Hash;
@@ -104,7 +104,7 @@ impl Drop for AddressCache {
 ///
 fn update_balance(
     block: SConnectedBlock,
-    balance: &mut BTreeMap<usize, i64>,
+    balance: &mut HashMap<usize, i64>,
     cache: &mut AddressCache,
 ) {
     for tx in block.txdata {
@@ -134,7 +134,7 @@ fn update_balance(
     }
 }
 
-fn write_balance(balance: &BTreeMap<usize, i64>, out_dir: &Path, date: Date<Utc>) {
+fn write_balance(balance: &HashMap<usize, i64>, out_dir: &Path, date: Date<Utc>) {
     let file_name = date.format("%Y-%m-%d").to_string() + ".csv";
     let mut out_file = out_dir.to_path_buf();
     out_file.extend(Path::new(&file_name));
@@ -170,7 +170,7 @@ fn main() {
         "[{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos:>10}/{len:10} ({per_sec}, {eta})",
     ));
 
-    let (sender, receiver) = mpsc::sync_channel::<(BTreeMap<usize, i64>, Date<Utc>)>(10);
+    let (sender, receiver) = mpsc::sync_channel::<(HashMap<usize, i64>, Date<Utc>)>(10);
 
     // start writer thread
     let writer = thread::spawn(move || {
@@ -183,7 +183,7 @@ fn main() {
     let producer = thread::spawn(move || {
         // initialize
         let mut address_cache = AddressCache::new(out_dir);
-        let mut bal_change: BTreeMap<usize, i64> = BTreeMap::new();
+        let mut bal_change: HashMap<usize, i64> = HashMap::new();
         let mut prev_date: Option<Date<Utc>> = None;
 
         for blk in db.iter_connected_block::<SConnectedBlock>(end as u32) {
@@ -194,7 +194,7 @@ fn main() {
                     sender
                         .send((bal_change.clone(), prev_date.clone()))
                         .unwrap();
-                    bal_change = BTreeMap::new();
+                    bal_change = HashMap::new();
                 }
             }
             prev_date = Some(date);
