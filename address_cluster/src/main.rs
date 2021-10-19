a500749990a769bcc1c8e01c7b46345ed0835a00
Congyuwang <congyuwang@link.cuhk.edu.cn>
2021-10-19 11:28:58 +0800

    load all addresses in advance to prepare for H2

--- address_cluster/src/main.rs
+++ address_cluster/src/main.rs
@@ -1,11 +1,13 @@
 use ahash::AHasher;
-use bitcoin_explorer::{Address, BitcoinDB, SConnectedBlock};
+use bitcoin_explorer::{Address, BitcoinDB, SBlock, SConnectedBlock};
+use ena::unify::{InPlaceUnificationTable, UnifyKey};
 use hash_hasher::HashedMap;
 use indicatif;
 use indicatif::ProgressStyle;
 use log::{info, LevelFilter};
 use par_iter_sync::IntoParallelIteratorSync;
 use simple_logger::SimpleLogger;
+use std::collections::hash_map::Entry;
 use std::fs;
 use std::fs::File;
 use std::hash::{Hash, Hasher};
@@ -15,7 +17,6 @@ use std::sync::mpsc::Sender;
 use std::sync::{mpsc, Arc, Mutex, MutexGuard};
 use std::thread;
 use std::thread::JoinHandle;
-use ena::unify::{InPlaceUnificationTable, UnifyKey};
 
 #[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
 struct UnitKey(u32);
@@ -78,7 +79,8 @@ impl AsyncBufWriter {
 
 #[derive(Clone)]
 struct AddressCache {
-    address_index: Arc<Mutex<HashedMap<u128, u32>>>,
+    // (index, count)
+    address_index: Arc<Mutex<HashedMap<u128, (u32, u32)>>>,
     union_find: Arc<Mutex<InPlaceUnificationTable<UnitKey>>>,
     permanent_store: AsyncBufWriter,
 }
@@ -108,26 +110,51 @@ impl AddressCache {
         }
     }
 
+    pub fn connect_addresses_index(&self, addresses_index: &Vec<u32>) {
+        if addresses_index.len() > 1 {
+            let first = addresses_index.first().unwrap();
+            for rest in addresses_index.iter().skip(1) {
+                // connect rest to first
+                self.union(*first, *rest);
+            }
+        }
+    }
+
     #[inline]
-    pub fn try_get_or_add_address_index(&self, addresses: Box<[Address]>) -> Option<u32> {
+    pub fn add_new_address(&self, addresses: Box<[Address]>) {
         if let Some(addresses_string) = Self::addresses_to_string(addresses) {
             let address_hash = Self::hash(&addresses_string);
             let mut is_new_address = false;
+            // cache lock scope
             let index = {
                 let mut cache = self.address_index.lock().unwrap();
                 let new_index = cache.len() as u32;
-                *cache.entry(address_hash).or_insert_with(|| {
-                    is_new_address = true;
-                    self.union_find.lock().unwrap().new_key(());
-                    new_index
-                })
+                let entry = cache.entry(address_hash);
+                let (index, _) = match entry {
+                    Entry::Occupied(mut o) => {
+                        let (index, count) = o.get();
+                        o.insert((*index, count + 1))
+                    }
+                    Entry::Vacant(v) => {
+                        *v.insert((new_index, 1))
+                    }
+                };
+                index
             };
             if is_new_address {
                 let line = (index.to_string() + "," + &addresses_string + "\n").into_bytes();
                 // sync
                 self.permanent_store.write_all(line.into_boxed_slice());
             }
-            Some(index)
+        }
+    }
+
+    pub fn assert_get_address_index(&self, addresses: Box<[Address]>) -> Option<u32> {
+        if let Some(address_string) = AddressCache::addresses_to_string(addresses) {
+            Some(
+                self.get_address_index(AddressCache::hash(&address_string))
+                    .expect("inputs must exist in UTXO"),
+            )
         } else {
             None
         }
@@ -164,7 +191,7 @@ impl AddressCache {
             .lock()
             .unwrap()
             .get(&hash)
-            .map(|x| x.to_owned())
+            .map(|(x, _)| x.to_owned())
     }
 
     #[inline]
@@ -199,7 +226,10 @@ impl AddressCache {
 }
 
 fn main() {
-    SimpleLogger::new().with_level(LevelFilter::Info).init().unwrap();
+    SimpleLogger::new()
+        .with_level(LevelFilter::Info)
+        .init()
+        .unwrap();
     let db = BitcoinDB::new(Path::new("/116020237/bitcoin"), false).unwrap();
     let end = db.get_block_count();
     let out_dir = Path::new("./out/balances/");
@@ -208,6 +238,7 @@ fn main() {
     }
     info!("launching DB finished");
 
+    info!("load all addresses");
     // preparing progress bar
     let total_number_of_transactions = (0..end)
         .map(|i| db.get_header(i).unwrap().n_tx)
@@ -220,28 +251,49 @@ fn main() {
     let mut address_cache = AddressCache::new(out_dir);
     let address_writer_handle = address_cache.pop_handle();
 
+    // load all addresses
+    let address_cache_clone = address_cache.clone();
+    db.iter_block::<SBlock>(0, end)
+        .into_par_iter_sync(move |blk| {
+            let len = blk.txdata.len();
+            for tx in blk.txdata {
+                for o in tx.output {
+                    address_cache_clone.add_new_address(o.addresses);
+                }
+            }
+            Ok(len)
+        })
+        .for_each(|p| bar.inc(p as u64));
+    info!("finished loading all addresses");
+
+    // preparing progress bar
+    let total_number_of_transactions = (0..end)
+        .map(|i| db.get_header(i).unwrap().n_tx)
+        .sum::<u32>() as u64;
+    let bar = indicatif::ProgressBar::new(total_number_of_transactions);
+    bar.set_style(ProgressStyle::default_bar().progress_chars("=>-").template(
+        "[{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos:>10}/{len:10} ({per_sec}, {eta})",
+    ));
+
     // producer thread
     let producer = thread::spawn(move || {
-
         let mut address_cache_clone = address_cache.clone();
         let par_iter = db
             .iter_connected_block::<SConnectedBlock>(end)
             .into_par_iter_sync(move |blk| {
-                // H1: common spending logic
+                // let mut count = 0;
                 for tx in &blk.txdata {
                     let out_addresses: Vec<u32> = tx
                         .output
                         .iter()
-                        .map(|o| address_cache.try_get_or_add_address_index(o.addresses.clone()))
+                        .map(|o| address_cache_clone.assert_get_address_index(o.addresses.clone()))
                         .filter_map(|x| x)
                         .collect();
-                    if out_addresses.len() > 1 {
-                        let first = out_addresses.first().unwrap();
-                        for rest in out_addresses.iter().skip(1) {
-                            address_cache.union(*first, *rest);
-                            assert!(address_cache.connected(*first, *rest));
-                        }
-                    }
+                    // H3: coinbase tx
+                    // if count == 0 {
+                    //     address_cache.connect_addresses_index(&out_addresses);
+                    // }
+                    // count += 1;
                 }
                 Ok(blk)
             })
@@ -251,22 +303,11 @@ fn main() {
                     let in_addresses: Vec<u32> = tx
                         .input
                         .iter()
-                        .map(|i| {
-                            if let Some(address_string) =
-                                AddressCache::addresses_to_string(i.addresses.clone())
-                            {
-                                Some(
-                                    address_cache_clone
-                                        .get_address_index(AddressCache::hash(&address_string))
-                                        .expect("inputs must exist in UTXO"),
-                                )
-                            } else {
-                                None
-                            }
-                        })
+                        .map(|i| address_cache.assert_get_address_index(i.addresses.clone()))
                         .filter_map(|x| x)
                         .collect();
-                    // TODO!("check OTC")
+                    // H1: common spending
+                    address_cache.connect_addresses_index(&in_addresses);
                 }
                 Ok(progress)
             })
